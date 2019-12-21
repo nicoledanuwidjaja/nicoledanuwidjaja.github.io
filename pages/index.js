ab88c80537b00a0a5bccf0f26d405c61f22eb6d9
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-12-21 12:50:23 -0800

    starter web files

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+// React component
+export default function Index() {
+   return (
+      <Layout>
+      	   <p>Hello Next.js</p>
+      </Layout>
+   );
+}
