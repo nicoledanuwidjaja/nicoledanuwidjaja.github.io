c380a5d9ec7f18caf91a2bff3b587e29c79a9888
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-05-16 03:21:34 -0400

    projects styling, animations, and tag layouts

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const getProjects = () => {
@@ -9,48 +9,55 @@ const getProjects = () => {
             technologies: ['GraphQL', 'React', 'Node', 'Bookshelf', 'Knex', 'PostgreSQL']
         },
         {id: 'scrapetheprez', title: 'ScrapeThePrez', technologies: ['Python', 'Firebase']},
-        {id: 'tricone', title: 'Tricone', technologies: ['Arduino BLE']},
+        {id: 'tricone', title: 'Tricone', technologies: ['Arduino BLE', 'Vue.js']},
         {id: 'garbadoor', title: 'GarbaDoor', technologies: ['Python', 'React', 'Arduino', 'Firebase']},
-        {id: 'cantrip', title: 'CanTrip', technologies: ['Java', 'Firebase']},
-        {id: 'test6', title: 'Project 6', technologies: ['React']}
+        {id: 'cantrip', title: 'CanTrip', technologies: ['Java', 'Firebase', 'MySQL']},
+        {id: 'test6', title: 'Fable', technologies: ['Swift', 'Kotlin', 'React']}
     ];
 };
 
-const ProjectLink = ({project}) => (
-    <div className="project-container">
-        <Link href="/project/[id]" as={`/project/${project.id}`}>
-            <div className="project-pic"/>
-        </Link>
-        <div className="project-info">
-            <div className="project-name">
-                <a>{project.title}</a>
-            </div>
-            <div className="caption-container">
-                {project.technologies.map((technology) =>
-                    <div className="project-caption">
-                        <p>{technology}</p>
-                    </div>
-                )}
+const ProjectLink = ({project}) => {
+    const [isShowing, showTags] = useState(false);
+
+    return (
+        <div className="project-container">
+            <Link href="/project/[id]" as={`/project/${project.id}`}>
+                <div className="project-pic"
+                     onMouseEnter={() => showTags(true)}
+                     onMouseLeave={() => showTags(false)}>
+                    {
+                        (<div className="caption-container">
+                            {project.technologies.map((technology) =>
+                                <div className="project-caption">
+                                    <p>{technology}</p>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+            </Link>
+            <div className="project-info">
+                <div className="project-name">
+                    <a>{project.title}</a>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+}
 
-class Projects extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="container-header">
-                    <div className="container-title">Projects</div>
-                </div>
-                <div className="projects">
-                    {getProjects().map(project => (
-                        <ProjectLink key={project.id} project={project}/>
-                    ))}
-                </div>
+const Projects = (props) => {
+    return (
+        <div className="container">
+            <div className="container-header">
+                <div className="container-title">Projects</div>
             </div>
-        );
-    }
+            <div className="projects">
+                {getProjects().map(project => (
+                    <ProjectLink key={project.id} project={project}/>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
