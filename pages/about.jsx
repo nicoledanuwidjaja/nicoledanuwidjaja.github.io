bbb74734c8f9e6a1e3c0715f1aeebbbcb633d354
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-11-26 14:35:12 -0800

    deploy to pages

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,12 +1,12 @@
 import Layout from '../components/Layout';
-import {Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+// import {Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
 
 export default function About() {
    return(
      <>
     <div className="content">
   	  <Layout>
-        <Element name="about" className="container">
+        <div name="about" className="container">
           <h1>What is dis?</h1>
           <p>This website was made on a whim on December 22, 2019. I desperately need a portfolio -- mainly because my co-op at <a href="http://whoop.com/">WHOOP</a> as a <strong>Front-End Developer</strong> starts in T-14 days and I don't even have a presentable portfolio.</p>
           <p>Plenty of more updates to come... real soon!</p>
@@ -16,7 +16,7 @@ export default function About() {
             <li>Next.js</li>
             <li>JSX</li>
           </ul>
-        </Element>
+        </div>
 
         <style jsx global>{`
           .content {
